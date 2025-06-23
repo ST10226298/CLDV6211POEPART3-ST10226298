b7ac1b86c8aead8cf3d05b4d7ce3dbdd98b16409
ST10226298 <102595208+ST10226298@users.noreply.github.com>
2025-06-23 14:29:45 +0200

    Add files via upload
    Views and more

---
+++ BlobService.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Storage.Blobs;
+
+namespace EventEaseBookingSystem.Services
+{
+    public class BlobService : IBlobService
+    {
+        private readonly IConfiguration _configuration;
+
+        public BlobService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<string> UploadFileAsync(IFormFile file)
+        {
+            var blobConnectionString = _configuration["AzureStorage:BlobConnectionString"];
+            var containerName = _configuration["AzureStorage:ContainerName"];
+
+            var blobServiceClient = new BlobServiceClient(blobConnectionString);
+            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            var blobClient = containerClient.GetBlobClient(Guid.NewGuid().ToString() + Path.GetExtension(file.FileName));
+
+            using (var stream = file.OpenReadStream())
+            {
+                await blobClient.UploadAsync(stream, overwrite: true);
+            }
+
+            return blobClient.Uri.ToString();
+        }
+    }
+}
---
+++ BookingDisplayViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace EventEaseBookingSystem.ViewModels
+{
+    public class BookingDisplayViewModel
+    {
+        public int BookingId { get; set; }
+        public string EventName { get; set; }
+        public string VenueName { get; set; }
+        public string EventTypeName { get; set; }
+        public DateTime Date { get; set; }
+        public bool VenueAvailable { get; set; }
+
+        // Filters/Search
+        public string SearchTerm { get; set; }
+        public int? SelectedEventTypeId { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
